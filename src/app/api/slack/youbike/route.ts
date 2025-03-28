5ff2946befce104f682db3caa0fff64372b80e9e
tli-gini <ginitong.fish0524@gmail.com>
2025-03-28 13:57:35 +0800

    fetching api

---
+++ src/app/api/slack/youbike/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const response = await fetch(`${process.env.Base_URL}/api/youbike`);
+  const stations = await response.json();
+
+  if (!stations.length) {
+    return NextResponse.json({
+      response_type: "ephemeral",
+      text: "抱歉，我現在找不到站點。",
+    });
+  }
+
+  const imageUrl = `ubike-info\public\images\happy puppy.png`;
+
+  const blocks = stations.flatMap((station: any) => [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*:round_pushpin: ${station.sna}*\n:bicycle: *可借車輛:* ${station.bikesAvailable}\n:parking: *可停空位:* ${station.parkingAvailable}`,
+      },
+      accessory:{
+        type: "image",
+        image_url: imageUrl,
+        alt_text: "cute puppy",
+      }
+    },
+    {
+      type: "context",
+      elements: [
+        {
+          type: "mrkdwn",
+          text: `更新時間：${formatTimestamp(station.lastUpdated)}`,
+        },
+      ],
+    },
+    { type: "divider" },
+  ]);
+
+  return NextResponse.json({
+    response_type: "in_channel",
+    blocks,
+  });
+}
+
+// format timestamps clearly
+function formatTimestamp(timestamp: string): string {
+  const year = timestamp.substring(0, 4);
+  const month = timestamp.substring(4, 6);
+  const day = timestamp.substring(6, 8);
+  const hour = timestamp.substring(8, 10);
+  const minute = timestamp.substring(10, 12);
+  const second = timestamp.substring(12, 14);
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
---
+++ src/app/api/youbike/route.ts
@@ -0,0 +1,23 @@
+ // app/api/youbike/route.ts
+ import { NextResponse } from 'next/server';
+
+ export async function GET() {
+   const res = await fetch('https://data.ntpc.gov.tw/api/datasets/010e51b5-3823-4b20-b401-b1cf000550c5/json?page=0&size=100');
+   const data = await res.json();
+ 
+   const targetStations:Record<string, string> = {
+    "YouBike2.0_五工二五工一路口(泰博科技)":"泰博科技",
+    "YouBike2.0_捷運新北產業園區站":"捷運新北產業園區"
+};
+ 
+   const stationData = data
+     .filter((station: any) => Object.keys(targetStations).includes(station.sna))
+     .map((station: any) => ({
+       sna: targetStations[station.sna],
+       bikesAvailable: station.sbi,
+       parkingAvailable: station.bemp,
+       lastUpdated: station.mday,
+     }));
+ 
+   return NextResponse.json(stationData);
+ }
